72b89aead704c9b199253e3e6768f14da13d2e69
Ruslan Voroshchuk <ruslan@owlookit.com>
2021-01-17 14:17:42 +0200

    get the submit and go to the next step logic separated, labels moved to computed

--- vue.js
+++ vue.js
@@ -5,6 +5,7 @@ const App = {
   data() {
     return {
       activeIndex: 0, // то, что позволяет определить текущий активный шаг
+      isFinished: false, // то, что позволяет определить выполнен последний шаг
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -13,9 +14,10 @@ const App = {
         {title: 'Composition', text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'},
       ],
       statics: {
-        labelButtonPrev: "Назад",
-        labelButtonNext: "Вперед",
-        labelButtonReset: "Начать заново",
+        labelButtonPrev:     "Назад",
+        labelButtonNext:     "Вперед",
+        labelButtonFinished: "Закончить",
+        labelButtonReset:    "Начать заново",
       }
     }
   },
@@ -28,26 +30,25 @@ const App = {
     reset() {
       // начать заново
       this.activeIndex = 0;
-      this.updateNextLabel();
+      this.isFinished = false;
       console.log('начать заново');
     },
-    updateNextLabel() {
-      if (this.activeIndex >= this.steps.length - 1) {
-        this.statics.labelButtonNext = "Закончить";
-      } else {
-        this.statics.labelButtonNext = "Вперед";
-      }
-    },
-    nextOfFinish() {
+    nextOrFinish() {
       // кнопка вперед или закончить
+      if (this.activeIndex >= this.steps.length - 1) {
+        this.submit();
+        this.isFinished = true;
+        return;
+      }
       ++this.activeIndex;
-      this.updateNextLabel();
       console.log('кнопка вперед или закончить');
+    },
+    submit() {
+      console.log('данные отправлены');
     },
     setActive(idx) {
       // когда нажимаем на определенный шаг
       this.activeIndex = idx;
-      this.updateNextLabel();
       console.log('когда нажимаем на определенный шаг');
     }
   },
@@ -55,7 +56,7 @@ const App = {
     // тут стоит определить несколько свойств:
     // 1. текущий выбранный шаг
     getActiveIndex() {
-      return (this.activeIndex < this.steps.length) ? this.activeIndex : (this.steps.length - 1);
+      return this.activeIndex;
     },
     // 2. выключена ли кнопка назад
     isFirstStep() {
@@ -64,6 +65,19 @@ const App = {
     // 3. находимся ли мы на последнем шаге
     isLastStep() {
       return (this.activeIndex >= (this.steps.length));
+    },
+    // 4. получаем названия кнопок
+    getNextLabel() {
+      if (this.activeIndex >= this.steps.length - 1) {
+        return this.statics.labelButtonFinished;
+      }
+      return this.statics.labelButtonNext;
+    },
+    getPrevLabel() {
+      return this.statics.labelButtonPrev;
+    },
+    getResetLabel() {
+      return this.statics.labelButtonReset;
     }
 
   }
